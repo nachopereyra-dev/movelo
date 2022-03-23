f8cb82fc04709930db2dbac09413d37150176c58
NachoPN <nachopereyranoguez@gmail.com>
2022-03-23 16:24:02 -0300

    home-mobile terminado

--- app.js
+++ app.js
@@ -10,3 +10,19 @@ app.use(express.static('public'))
 app.get('/', function(req, res) {
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 })
+
+app.get('/servicios', function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+})
+
+app.get('/shop', function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+})
+
+app.get('/login', function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.get('/registro', function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
