fb94d67d4a07f89e931aab767f37800ce90f2527
Vampi29 <aleroblesdm@gmail.com>
2022-04-07 01:11:45 -0300

    servicios.ejs con publicaciones dinamicas.  agregados 6 (seis) 'productos de envio' o articulos en productsControllers.js

--- Movelo (dinamico)/controllers/productsController.js
+++ Movelo (dinamico)/controllers/productsController.js
@@ -1,24 +1,85 @@
 const listadoDeProductos = [
     {
         id: 1,
+        verificado: true,
         mensajero: 'Juan Perez',
-        partida: 'Buenos Aires',
+        origen: 'Buenos Aires',
         destino: 'Mar del Plata',
         categoria: 'categoria',
         distancia: 20,//km
         peso: 2,//kg
-        precio: 100,//u$s
-        imagen: 'nombre-de-imagen.jpg',
+        precio: '14,99',//u$s
+        imagen: 'BA-MDQ.png',
+    },
+    {
+        id: 2,
+        verificado: true,
+        mensajero: 'Daniel Klein',
+        origen: 'Palermo',
+        destino: 'Lanus',
+        categoria: 'categoria',
+        distancia: 20,//km
+        peso: 2,//kg
+        precio: '4,60',//u$s
+        imagen: 'BA-MDQ.png',
+    },
+    {
+        id: 3,
+        verificado: false,
+        mensajero: 'Carlos Manson',
+        origen: 'Buenos Aires',
+        destino: 'Punta del Este',
+        categoria: 'categoria',
+        distancia: 20,//km
+        peso: 2,//kg
+        precio: '22,49',//u$s
+        imagen: 'BA-MDQ.png',
+    },
+    {
+        id: 4,
+        verificado: true,
+        mensajero: 'Julián Suárez',
+        origen: 'Montevideo',
+        destino: 'Buenos Aires',
+        categoria: 'categoria',
+        distancia: 20,//km
+        peso: 2,//kg
+        precio: '16,20',//u$s
+        imagen: 'BA-MDQ.png',
+    },
+    {
+        id: 5,
+        verificado: false,
+        mensajero: 'Camila Saraceno',
+        origen: 'Belgrano',
+        destino: 'La Plata',
+        categoria: 'categoria',
+        distancia: 20,//km
+        peso: 2,//kg
+        precio: '7,99',//u$s
+        imagen: 'BA-MDQ.png',
+    },
+    {
+        id: 6,
+        verificado: true,
+        mensajero: 'Joel Cohen',
+        origen: 'Rosario',
+        destino: 'Mar del Plata',
+        categoria: 'categoria',
+        distancia: 20,//km
+        peso: 2,//kg
+        precio: '20,99',//u$s
+        imagen: 'BA-MDQ.png',
     }
 ]
 
 
 const productsController = {
     servicios: (req, res) => {
-        res.render("products/servicios", { title: 'Express' })
+        res.render("products/servicios", { articulos: listadoDeProductos })
     },
     carrito: (req, res) => {
-        res.render("products/carrito", { title: 'Express' })
+        res.render("products/carrito", { articulos: listadoDeProductos })
     }
 }
 
