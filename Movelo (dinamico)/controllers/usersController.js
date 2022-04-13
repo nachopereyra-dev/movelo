1cb5e52b123c488881495fc983547bf29fde945e
Vampi29 <aleroblesdm@gmail.com>
2022-04-12 21:40:03 -0300

    agregado admin/editar, admin/crear

--- Movelo (dinamico)/controllers/usersController.js
+++ Movelo (dinamico)/controllers/usersController.js
@@ -8,10 +8,12 @@ const userController = {
     registro: (req, res) => {
         res.render("users/registro", { title: 'Express' })
     },
-    admin: (req, res) => {
-        res.render("users/admin", { title: 'Express' })
+    adminCrear: (req, res) => {
+        res.render("users/admin-crear", { title: 'Express' })
+    },
+    adminEditar: (req, res) => {
+        res.render("users/admin-editar", { title: 'Express' })
     }
-    
 }
 
 module.exports = userController;
--- Movelo (dinamico)/routes/users.js
+++ Movelo (dinamico)/routes/users.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/usersController');
 /* GET users listing. */
 router.get('/login', userController.login);
 router.get('/registro', userController.registro);
-router.get('/admin', userController.admin);
+router.get('/admin/crear', userController.adminCrear);
+router.get('/admin/editar', userController.adminEditar);
 
 module.exports = router;
