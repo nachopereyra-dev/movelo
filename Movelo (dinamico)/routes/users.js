4a0d72cf17524a5c2a4a1d148c7f88594c720808
NachoPN <nachopereyranoguez@gmail.com>
2022-05-14 17:48:31 -0300

    Configurando subida de foto de perfil

--- Movelo (dinamico)/routes/users.js
+++ Movelo (dinamico)/routes/users.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+const path = require('path');
 var { body } = require('express-validator');
 
 
 
 const storage = multer.diskStorage({ 
     destination: function (req, file, cb) { 
-       cb(null, './public/images/avatars'); 
+       cb(null, path.join(__dirname, '../public/images/avatars')); 
     }, 
     filename: function (req, file, cb) { 
-       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  } 
-  })
+       cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`)
+  }
+});
+
 const userController = require('../controllers/usersController');
-const uploadFile = multer({ storage });
+const fileUpload = multer({ storage: storage });
 
 const validacion = [
    body('name').notEmpty().withMessage('Debes ingresar tu nombre completo'),
@@ -31,7 +34,7 @@ router.get('/registro', userController.registro);
 router.get('/admin/crear', userController.adminCrear);
 router.get('/admin/elegir-editar', userController.adminEditar);
 
-router.post('/registro', uploadFile.single('avatar'), validacion, userController.crearUsuario);
+router.post('/registro', fileUpload.single('avatar'), validacion, userController.crearUsuario);
 router.post('/login', [
    body('email').isEmail().withMessage('Debes ingresar un correo electrónico valido'),
    body('password').isLength({min: 8}).withMessage('Debes ingresar una contraseña de al menos 8 caracteres')
