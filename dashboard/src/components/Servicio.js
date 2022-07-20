01e3cf1f00d982736db457365cf21611d66ef9d1
Alejandro Robles <99698025+Vampi29@users.noreply.github.com>
2022-07-19 22:08:57 -0300

    Revert "corrigiendo"
    This reverts commit 5aafa2534e3327b78cb12aab7dbb77330aa0d803.

--- dashboard/src/components/Servicio.js
+++ dashboard/src/components/Servicio.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Servicio(props){
     return(
         <>
-            <div className="col-lg-6 mb-4">
+            <div className="col-lg-12 mb-2">
                 <div className="card text-white bg-dark  shadow">
-                    <div className="card-body">
-                        <p>Desde - {props.origen}</p>
-                        <p>Hacia - {props.destination} </p>
-                    </div>
+                        <Link style={{color: 'white'}} to={"/"}>
+                        <div className="card-body" style={{padding: 5, display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <div style={{margin:0}}># <strong>{props.id_service}</strong> | Desde <strong>{props.origen}</strong> - Hacia <strong>{props.destination}</strong></div>
+                            
+                                <div style={{display: 'flex'}}>Ver más</div>
+                            
+                        </div>
+                        </Link>
                 </div>
             </div>
         </>
