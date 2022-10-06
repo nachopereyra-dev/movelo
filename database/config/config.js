c86feefe308c96819a68647dc35430075093685e
Juan Ignacio Pereyra Noguez <nacho@MacBook-Air-de-Juan.local>
2022-10-06 11:30:42 -0300

    corrigiendo organizacion de carpetas

---
+++ database/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "b39634dd99f724",
+    "password": '386f5c3f',
+    "database": "heroku_ac625ad258a6888",
+    "host": "us-cdbr-east-06.cleardb.net",
+    "dialect": "mysql",
+    "operatorsAliases": 0
+  },
+  "test": {
+    "username": "root",
+    "password": '',
+    "database": "movelo_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": 0
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": 0
+  }
+}
