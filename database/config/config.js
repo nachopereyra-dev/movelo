be1d53bcd73eeaff966204dbb5e2112d06df32c9
Juan Ignacio Pereyra Noguez <nacho@MacBook-Air-de-Juan.local>
2022-10-06 12:50:14 -0300

    probando deploy 2

--- database/config/config.js
+++ database/config/config.js
@@ -8,19 +8,27 @@ module.exports = {
     "operatorsAliases": 0
   },
   "test": {
-    "username": "root",
-    "password": '',
-    "database": "movelo_db",
-    "host": "127.0.0.1",
+    "username": "b39634dd99f724",
+    "password": '386f5c3f',
+    "database": "heroku_ac625ad258a6888",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql",
     "operatorsAliases": 0
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b39634dd99f724",
+    "password": '386f5c3f',
+    "database": "heroku_ac625ad258a6888",
+    "host": "us-cdbr-east-06.cleardb.net",
     "dialect": "mysql",
     "operatorsAliases": 0
   }
 }
+
+// "production": {
+//   "username": "root",
+//   "password": null,
+//   "database": "database_production",
+//   "host": "127.0.0.1",
+//   "dialect": "mysql",
+//   "operatorsAliases": 0
