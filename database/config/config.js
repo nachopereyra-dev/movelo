63ca746283c2a050e9f1dbe87c6eb06b7e17a443
Juan Ignacio Pereyra Noguez <nacho@MacBook-Air-de-Juan.local>
2022-10-06 15:47:44 -0300

    deploy pendiente

--- database/config/config.js
+++ database/config/config.js
@@ -8,22 +8,19 @@ module.exports = {
     "operatorsAliases": 0
   },
   "test": {
-    "username": "b39634dd99f724",
-    "password": '386f5c3f',
-    "database": "heroku_ac625ad258a6888",
-    "host": "us-cdbr-east-06.cleardb.net",
+    "username": "root",
+    "password": '',
+    "database": "movelo_db",
+    "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": 0
   },
   "production": {
-"use_env_variable": "mysql://b39634dd99f724:386f5c3f@us-cdbr-east-06.cleardb.net/heroku_ac625ad258a6888?reconnect=true"
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": 0
   }
 }
-
-// "production": {
-//   "username": "root",
-//   "password": null,
-//   "database": "database_production",
-//   "host": "127.0.0.1",
-//   "dialect": "mysql",
-//   "operatorsAliases": 0
